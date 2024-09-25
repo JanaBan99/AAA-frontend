12cfb979b0b8a1955d433d9e0e2d1ca2b1acdc44
Ruwan5 <ruwannawarathna96@gmail.com>
2024-09-25 13:17:59 +0530

    sidebar topic chaged

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -22,7 +22,7 @@
                 </a>
             </li>
             <li class="nav-item mt-3">
-                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Accounts</h6>
+                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Chats</h6>
             </li>
 
             <li class="nav-item">
